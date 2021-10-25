451c18463f3225c2301146dd541255ea51e4d02f
Npatel <np395@njit.edu>
2021-10-25 12:45:18 -0400

    initial commit of user table

---
+++ user.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE it490forumDB;
+
+
+CREATE TABLE Users (
+        username varchar(255)
+        Lastname varchar(255)
+        FirstName varchar(255)
+        Password varchar(255)
+        email varchar(255)
+);
+
+
