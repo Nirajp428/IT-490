a59c272547fe161ebbdb5e2f88b7dc321032be6e
Chirag Kumar <Ck264@njit.edu>
2021-10-31 14:04:58 -0700

    Update db.php, login.php, mysqlconnect.php, nav.php, register.php

--- partials/nav.php
+++ partials/nav.php
@@ -13,7 +13,6 @@ require_once(__DIR__ . "/../lib/helpers.php");
 
  <?php if (has_role("Admin")): ?>
         <?php endif; ?> 
-			 -->
 
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
