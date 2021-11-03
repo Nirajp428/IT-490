7a6f8d716a1aa2ce5fa87b42cde0027a94da931a
VenturiniM <50147928+VenturiniM@users.noreply.github.com>
2021-11-03 13:43:46 -0400

    Adding movie table
    Adding movie table sql file for storing movies pulled from the API

---
+++ createMovieTable.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Movies
+(
+	movieID	int NOT NULL AUTO_INCREMENT,
+	title VARCHAR(40),
+	year VARCHAR(10),
+	rated VARCHAR(10),
+	released VARCHAR(30),
+	runtime VARCHAR(15),
+	genre VARCHAR(20),
+	director VARCHAR(40),
+	actors VARCHAR(100),
+	plot VARCHAR(500),
+	poster VARCHAR(120),
+	imdbRating VARCHAR(10),
+	contentType VARCHAR(15),
+	seasons VARCHAR(5),
+	primary key (movieID)
+)
