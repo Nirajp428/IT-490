d1a386a0da12c8c51fa88e99e9747c523b59151d
VenturiniM <50147928+VenturiniM@users.noreply.github.com>
2021-11-07 17:45:46 -0500

    increased varchar values for most columns

--- createMovieTable.sql
+++ createMovieTable.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Movies
 (
 	movieID	int NOT NULL AUTO_INCREMENT,
-	title VARCHAR(40),
+	title VARCHAR(60),
 	year VARCHAR(10),
-	rated VARCHAR(10),
-	released VARCHAR(30),
-	runtime VARCHAR(15),
+	rated VARCHAR(20),
+	released VARCHAR(40),
+	runtime VARCHAR(20),
 	genre VARCHAR(60),
-	director VARCHAR(40),
+	director VARCHAR(60),
 	actors VARCHAR(100),
 	plot VARCHAR(500),
 	poster VARCHAR(120),
 	imdbRating VARCHAR(10),
-	contentType VARCHAR(15),
-	seasons VARCHAR(5),
+	contentType VARCHAR(20),
+	seasons VARCHAR(20),
 	primary key (movieID)
 )
