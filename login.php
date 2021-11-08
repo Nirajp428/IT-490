72801ce139b0c4859bd279c7265a0c4c7fdcf721
VenturiniM <50147928+VenturiniM@users.noreply.github.com>
2021-11-07 20:48:34 -0500

    login file sort of works
     login page receives user data but the login page stalls after.

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+require_once(__DIR__ . "/partials/nav.php");
+require_once('sendMessage.php');
+?>
     <form method="POST">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required/>
@@ -28,51 +31,15 @@ if (isset($_POST["login"])) {
         flash("Invalid email");
     }
     if ($isValid) {
-        $db = getDB();
-        if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email LIMIT 1");
-
-            $params = array(":email" => $email);
-            $r = $stmt->execute($params);
-            //echo "db returned: " . var_export($r, true);
-            $e = $stmt->errorInfo();
-            if ($e[0] != "00000") {
-                //echo "uh oh something went wrong: " . var_export($e, true);
-                flash("Something went wrong, please try again");
-            }
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($result && isset($result["password"])) {
-                $password_hash_from_db = $result["password"];
-                if (password_verify($password, $password_hash_from_db)) {
-                    $stmt = $db->prepare("SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id 
-			    		where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
-                    $stmt->execute([":user_id" => $result["id"]]);
-                    $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                    unset($result["password"]);//remove password so we don't leak it beyond this page
-                    //let's create a session for our user based on the other data we pulled from the table
-                    $_SESSION["user"] = $result;//we can save the entire result array since we removed password
-                    if ($roles) {
-                        $_SESSION["user"]["roles"] = $roles;
-                    }
-                    else {
-                        $_SESSION["user"]["roles"] = [];
-                    }
-                    //on successful login let's serve-side redirect the user to the home page.
-                    flash("Log in successful");
-                    die(header("Location: home.php"));
-                }
-                else {
-                    flash("Invalid password");
-                }
-            }
-            else {
-                flash("Invalid user");
-            }
-        }
-    }
-    else {
-        flash("There was a validation issue");
+	$response = sendLogin($email, $password);
+	if(isset ($response)){
+		flash("Login Successful");
+		$_SESSION["user"] = $response;
+		die(header("Location: home.php"));	
+	}
+	else{
+		flash("Invalid Credentials");
+	}
     }
 }
 ?>
