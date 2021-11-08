4c4125dc0f2a64c906487867ed736eed3f217d9b
Chirag Kumar <Ck264@njit.edu>
2021-11-07 18:53:10 -0800

    Update Nav.php

---
+++ movie.php
@@ -0,0 +1,33 @@
+<?php
+
+$movieID = "1";
+echo $movieID;
+
+$title = "Captain Marvel";
+echo $title;
+$year = "2019";
+
+$rated = "PG-13";
+
+$released = "March 08, 2019";
+
+$runtime = "123 min";
+
+$genre = "Action, Adventure, Sci-fi";
+
+$director = "Anna Boden, Ryan Fleck"; 
+
+$actors = "Brie Larson, Samuel L Jackson, Ben Mendelson";
+
+$plot = "Carol Danvers becomes one of the universes most heroes when Earth is attacked";
+
+$poster = "";
+
+$imdbRating = "9.2/10";
+
+$contentType = "";
+
+$seasons = "";
+
+?>
+
