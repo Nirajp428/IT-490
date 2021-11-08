cd90a4316ab6cc6816202725419006bacd1c1bd7
VenturiniM <50147928+VenturiniM@users.noreply.github.com>
2021-11-08 17:31:05 -0500

    Login now works

--- login.php
+++ login.php
@@ -31,14 +31,16 @@ if (isset($_POST["login"])) {
         flash("Invalid email");
     }
     if ($isValid) {
-	$response = sendLogin($email, $password);
-	if(isset ($response)){
+	  $response = sendLogin($email, $password);
+  
+	  if($response == false){
+		flash("invalid Credentials");
+		}
+
+	if($response["email"] = $email){
 		flash("Login Successful");
 		$_SESSION["user"] = $response;
 		die(header("Location: home.php"));	
-	}
-	else{
-		flash("Invalid Credentials");
 	}
     }
 }
