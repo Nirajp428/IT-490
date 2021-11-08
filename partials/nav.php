448eca8e2e98b598f24266ba3a976c152e55ae61
Ck264 <45633987+Ck264@users.noreply.github.com>
2021-11-07 21:57:30 -0500

    Updated nav.php
    Added movie information and watchparty link

--- partials/nav.php
+++ partials/nav.php
@@ -9,6 +9,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php if (!is_logged_in()): ?>
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
+        <li><a href="">Movie Information</a></li>
+        <li><a href="https://watchparty.me">WatchParty</a></li>
     <?php endif; ?>
 
 
