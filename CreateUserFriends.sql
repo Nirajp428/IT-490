dc32ce655d3977123d7c6eb8d0350a34ee8b7ecf
kevin <kevin.mould1@gmail.com>
2021-11-14 16:21:10 -0500

    Removed other VM server components

--- CreateUserFriends.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user_friends(
-        id int auto_increment,
-        user_id int,
-        friend_id int,
-	added boolean,
-        primary key (`id`),
-        foreign key (`user_id`) REFERENCES Users(`id`)
-)
-
--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -17,40 +17,10 @@ switch ($type) {
 		$request['message'] = $_SESSION['message'];
 		$client = new rabbitMQClient("testRabbitMQ.ini","logExchangeServer");
 		break;
-	case "login":
-		$request['type'] = $_SESSION['type'];
-		$request['email'] = $_SESSION['email'];
-		$request['password'] = $_SESSION['password'];
-		$client = new rabbitMQClient("testRabbitMQ.ini","dbServer");
-		break;
-	case "register":
-		$request['type'] = $_SESSION['type'];
-		$request['username'] = $_SESSION['username'];
-                $request['password'] = $_SESSION['password'];
-		$request['email'] = $_SESSION['email'];
-		$client = new rabbitMQClient("testRabbitMQ.ini","dbServer");
-		break;
-	case "request":
-		$request['type'] = $_SESSION['type'];
-		$request['movie'] = $_SESSION['movie'];
-		$client = new rabbitMQClient("testRabbitMQ.ini","dbServer");
-		break;
 	case "APIrequest":
 		$request['type'] = $_SESSION['type'];
                 $request['movie'] = $_SESSION['movie'];
                 $client = new rabbitMQClient("testRabbitMQ.ini","dmzServer");
-                break;
-	case "validateSession":
-		$request['type'] = $_SESSION['type'];
-		$request['sessionID'] = $_SESSION['sessionID'];
-                $client = new rabbitMQClient("testRabbitMQ.ini","dbServer");
-		break;
-
-	case "friend_request":
-                $request['type'] = $_SESSION['type'];
-		$request['id'] = $_SESSION['id'];
-		$request['friendUsername'] = $_SESSION['friendUsername'];
-                $client = new rabbitMQClient("testRabbitMQ.ini","dbServer");
                 break;
 
 }
@@ -59,9 +29,7 @@ $response = $client->send_request($request);
 //$response = $client->publish($request);
 
 echo "client received response: ".PHP_EOL;
-#print_r($response);
 return $response;
 echo "\n\n";
-
-#echo $argv[0]." END".PHP_EOL;
+?>
 
