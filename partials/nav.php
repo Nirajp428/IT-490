c753bfa560a21e99f2ab701cc54f11798776def9
VenturiniM <50147928+VenturiniM@users.noreply.github.com>
2021-12-05 14:19:28 -0500

    Adding folderto this branch from webServer

---
+++ partials/nav.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="static/css/styles.css">
+<?php
+
+require_once(__DIR__ . "/../lib/helpers.php");
+?>
+<nav>
+<ul class="nav">
+    <li><a href="home.php">Home</a></li>
+    <li><a href="movieList.php">Movies</a></li>
+    <li><a href="searchMovie.php">Search</a></li>
+    <?php if (!is_logged_in()): ?>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+   <?php endif; ?>
+
+
+    <?php if (is_logged_in()): ?>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="logout.php">Logout</a></li>
+        <li><a href="https://watchparty.me">WatchParty</a></li>
+    <?php endif; ?>
+</ul>
+</nav>
