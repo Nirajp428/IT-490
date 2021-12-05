bb6136c8d731b72cf42da3dd07d65ffa93df5573
VenturiniM <50147928+VenturiniM@users.noreply.github.com>
2021-12-05 14:20:00 -0500

    Adding partials folder to this branch

---
+++ partials/helpers.php
@@ -0,0 +1,59 @@
+<?php
+if (!isset($_SESSION)) { session_start(); }
+
+function is_logged_in() {
+    return isset($_SESSION["user"]);
+}
+
+
+function get_username() {
+    if (is_logged_in() && isset($_SESSION["user"]["username"])) {
+        return $_SESSION["user"]["username"];
+    }
+    return "";
+}
+
+function get_email() {
+    if (is_logged_in() && isset($_SESSION["user"]["email"])) {
+        return $_SESSION["user"]["email"];
+    }
+    return "";
+}
+
+function get_user_id() {
+    if (is_logged_in() && isset($_SESSION["user"]["id"])) {
+        return $_SESSION["user"]["id"];
+    }
+    return -1;
+}
+
+function safer_echo($var) {
+    if (!isset($var)) {
+        echo "";
+        return;
+    }
+    echo htmlspecialchars($var, ENT_QUOTES, "UTF-8");
+}
+
+//for flash feature
+function flash($msg) {
+    if (isset($_SESSION['flash'])) {
+        array_push($_SESSION['flash'], $msg);
+    }
+    else {
+        $_SESSION['flash'] = array();
+        array_push($_SESSION['flash'], $msg);
+    }
+
+}
+
+function getMessages() {
+    if (isset($_SESSION['flash'])) {
+        $flashes = $_SESSION['flash'];
+        $_SESSION['flash'] = array();
+        return $flashes;
+    }
+    return array();
+}
+//end flash
+?>
