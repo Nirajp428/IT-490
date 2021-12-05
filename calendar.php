d1bb7860159c478ce6f516aa6daf4ebffdfe87c0
Chirag Kumar <Ck264@njit.edu>
2021-12-05 14:08:57 -0800

    Implemented Google Calendar to the Front End

---
+++ calendar.php
@@ -0,0 +1,3 @@
+<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&ctz=America%2FNew_York&title=New%20Calendar&src=Y2hpcmFna3VtYXIzODVAZ21haWwuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%237986CB&color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+<a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=Nmk0NGc1cXYzZ2RsYW0zMTYybnN2N3Bwc2kgY2hpcmFna3VtYXIzODVAbQ&amp;tmsrc=chiragkumar385%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"></a>
+
